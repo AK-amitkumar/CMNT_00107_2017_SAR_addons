a445898fc75eb06600ff78852e68620c32d92ec6
javierjcf <javierjcf@gmail.com>
2017-07-05 19:52:51 +0200

    [DEL]custom_variant_grid: Eliminado módulo grid de la enterprise

--- custom_variant_grid/models/purchase.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2017 Comunitea Servicios Tecnológicos S.L. (http://comunitea.com)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
-from odoo import models, fields
-
-
-class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order.line'
