d5bbb0596bfe0c23c4cf7f94271ea9c1558ac2c2
Omar <omarcs7r@gmail.com>
2017-09-21 15:22:06 +0200

    [ADD] logistics_wip: Commit inicial sin instalar

---
+++ logistics_wip/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# © 2017 Comunitea Servicios Tecnológicos S.L. (http://comunitea.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': "Logistics WIP view",
+    'version': '10.0.1.0.0',
+    'category': 'Textile Vertical',
+    'author': 'Comunitea',
+    'website': 'http://www.comunitea.com',
+    'license': 'AGPL-3',
+    "depends": [
+        'textile_base',
+        'sale_stock',
+        'project_task_dependency',
+        'web_gantt',
+        'procurement_jit',
+        'mrp_mto_with_stock',
+        'mrp_auto_assign',
+        'stock_mts_mto_rule'
+    ],
+    "data": [],
+    "installable": True
+}
---
+++ logistics_wip/models/textile_model.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2017 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api
+
+
+class TextileModel(models.Model):
+
+    _inherit = "textile.model"
+
+    project_id = fields.Many2one("project.project", "Project", readonly=True)
+
+    @api.multi
+    def action_approve(self):
+        res = super(TextileModel, self).action_approve()
+        if self.model_type == 'model':
+            project = self.env["project.project"].\
+                create({'name': u'[' + self.reference + u'] ' + self.name,
+                        'label_tasks': 'Eventos',
+                        'partner_id': self.customer.id,
+                        'code': self.reference,
+                        'tag_ids': [(6, 0, [self.analytic_tag.id])]})
+            self.project_id = project.id
+        return res
