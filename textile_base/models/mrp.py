0c87b043af39a8f42687b850615c9e22adfa42b8
javierjcf <javierjcf@gmail.com>
2017-10-10 10:37:41 +0200

    [ADD]textile_base: Campo calculado peso % en las ldm

--- textile_base/models/mrp.py
+++ textile_base/models/mrp.py
@@ -2,15 +2,21 @@
 # © 2017 Comunitea
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class MrpBomLine(models.Model):
 
     _inherit = 'mrp.bom.line'
 
+    @api.multi
+    def _get_weight_per(self):
+        for line in self:
+            line.weight_per = 99.2
+
     bom_id = fields.Many2one(required=False)
     model_id = fields.Many2one('textile.model')
     model_type = fields.Selection(
         (('premodel', 'Pre-model'), ('model', 'Model')),
         related='model_id.model_type', readonly=True)
+    weight_per = fields.Float(compute='_get_weight_per', string='Weigth %')
